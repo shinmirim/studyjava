e75c500770ce04e42bd4f446537f45c91aea929a
shinmirim <90263843+shinmirim@users.noreply.github.com>
2022-07-15 00:02:26 +0900

    22.07.14
    수업 중 코드 완료

--- src/com/kosa/mirim/CoffeeMachineStart.java
+++ src/com/kosa/mirim/CoffeeMachineStart.java
@@ -4,12 +4,17 @@ public class CoffeeMachineStart {
 
 	public static void main(String[] args) {
 		
-		CoffeeMachine Latte = new Latte();
-		Latte.ice();
-		Latte.performshot();
-		Latte.performbase();
+		//인스턴스와 클래스를 잘구분하여 이름을 짓자. 인스턴스 접근시 소문자 클래스 접근시 대문
+		CoffeeMachine latte = new Latte();
+		latte.ice();
+		latte.performshot();
+	    latte.performbase();
+		
+		BaseBehavior syrup = new Syrup();//나는 바꿔야겠다 
+		latte.setBaseBehavior(syrup);//바꿔주세요 
+		latte.performbase();
 
-		Latte.setBaseBehavior(new Syrup());
+		//Latte.setBaseBehavior(new Syrup());
 	    System.out.println("완성");
 
 	
